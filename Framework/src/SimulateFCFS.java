e909649bf661c05bbaa79da68a47798c2fe95156
Sibusiso Mayalo <mylsbu001@myuct.ac.za>
2018-05-04 03:35:49 +0200

    Fixed compile issues

--- Framework/src/SimulateFCFS.java
+++ Framework/src/SimulateFCFS.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import simulator.ProcessControlBlock;
+import simulator.Config;
 
 public class SimulateFCFS
 {
@@ -10,19 +11,19 @@ public class SimulateFCFS
   private int syscall_cost;
   private int switch_cost;
   private int trace_level;
-  private Config configObject;
 
   public SimulateFCFS()
   {
     kernel = new FCFSKernel();
-    config = new Config();
-
     simulateMenu(); // print menu
 
-    config.init(kernel, this.switch_cost, this.syscall_cost) ;
-    kernel.setCPU(config.getCPU());
-    config.buildConfiguration(this.configFileName);
-    config.run();
+    Config.init(kernel, this.switch_cost, this.syscall_cost) ;
+    Config.buildConfiguration(this.configFileName);
+    Config.run();
+    System.out.println(resultMenu());
+    System.out.println("Context switches : " + Config.getCPU().getContextSwitches());
+    System.out.println("CPU utilization : " + (double)Config.getSystemTimer().getUserTime()/Config.getSystemTimer().getSystemTime()*100);
+
   }
 
   private void simulateMenu() // prints out the menu and get user input
@@ -30,32 +31,24 @@ public class SimulateFCFS
     this.input = new Scanner(System.in);
     System.out.println("*** FCFS Simulator ***");
 
-    System.out.print("enter configuration file name: ");
+    System.out.print("Enter configuration file name: ");
     this.configFileName = this.input.nextLine();
 
-    System.out.print("\nEnter cost of system call: ");
+    System.out.print("Enter cost of system call: ");
     this.syscall_cost = this.input.nextInt();
 
-    System.out.print("\nEnter cost of context switch: ");
+    System.out.print("Enter cost of context switch: ");
     this.switch_cost = this.input.nextInt();
 
-    System.out.print("\nEnter trace level: ");
+    System.out.print("Enter trace level: ");
     this.trace_level = this.input.nextInt();
 
   }
 
-  public void resultMenu(int sysTime, int kernelTime, int userTime, int idleTime, int contextTime, int utilTime)
+  public String resultMenu()
   {
     //print out the given results to the user
-
-    System.out.println("*** Results ***");
-    System.out.println("System time: " + sysTime);
-    System.out.println("Kernel time: " + kernelTime);
-    System.out.println("User time: " + userTime);
-    System.out.println("Idle time: " + idleTime);
-    System.out.println("Context switches: " + contextTime);
-    System.out.println("CPU utilization: " + utilTime);
-
+    return Config.getSystemTimer().toString();
   }
 
   public String toString() // returns all the values given by the user in the simulateMenu function
